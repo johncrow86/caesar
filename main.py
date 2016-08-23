f9a3c9b707f61ac349529ee81a521e11db92f1d0
John Crow <johncrow86@hotmail.com>
2016-08-23 11:20:17 -0500

    done. n/w

--- main.py
+++ main.py
@@ -16,9 +16,65 @@
 #
 import webapp2
 
+page_header = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ROT13</title>
+</head>
+<body>
+    <h1><b>Enter some text to ROT13</b></h1>
+"""
+
+page_footer = """
+</body>
+</html>
+"""
+
+ALPHABET_LOWERCASE = "abcdefghijklmnopqrstuvwxyz"
+ALPHABET_UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+textToEncrypt = ""
+
+def alphabet_position(letter):
+    alphabet = ALPHABET_LOWERCASE if letter.islower() else ALPHABET_UPPERCASE
+    return alphabet.index(letter)
+
+def rotate_char(char, rotation):
+    if not char.isalpha():
+        return char
+
+    alphabet = ALPHABET_LOWERCASE if char.islower() else ALPHABET_UPPERCASE
+    new_pos = (alphabet_position(char) + rotation) % 26
+    return alphabet[new_pos]
+
+def encrypt(text, rotation):
+    answer = ""
+    for char in text:
+        answer += rotate_char(char, rotation)
+    return answer
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+
+        rotNumber = 0
+
+        main_content = """
+        <form action="/">
+            <label>Enter text to encrypt</label>
+            <input name="text-rot" value="%(textToEncrypt)s"><br>
+            <label>Enter a number to rotate by</label>
+            <input name="num-rot"><br>
+            <input type=submit>
+        </form>
+        """
+
+        textToEncrypt = self.request.get("text-rot")
+        rotNumber = self.request.get("num-rot")
+        textToEncrypt = encrypt(textToEncrypt, int(rotNumber))
+
+        response = page_header + main_content + page_footer
+        self.response.write(response)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
